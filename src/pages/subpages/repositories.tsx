8d1c177e35c913e32b0282aacbc50e0bbd021ffe
Cristhian Felipe <cristhianfelipeyt@gmail.com>
2024-05-18 12:12:39 -0300

    hotfix/repositories

--- src/pages/subpages/repositories.tsx
+++ src/pages/subpages/repositories.tsx
@@ -51,7 +51,7 @@ export default function Repositories() {
       <div className="flex justify-center items-center w-full h-full overflow-x-none p-4 overflow-y-auto">
         <ul
           role="list"
-          className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+          className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-full"
         >
           {repositories.map((repository: Repository) => {
             return (
